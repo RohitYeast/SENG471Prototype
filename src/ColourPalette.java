978fd66e29094e19a2e27bd569e7258644af775a
Rohit Yeast <rohityeast@gmail.com>
2021-04-09 20:27:38 -0700

    COLOURS ADDED

--- src/ColourPalette.java
+++ src/ColourPalette.java
@@ -1,9 +1,52 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
-public class ColourPalette {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+@SuppressWarnings("serial")
+public class ColourPalette extends JFrame{
 
+	
+	
+	
+	public ColourPalette(ArrayList <Color> colors)
+	{
+		super("Color Palette Selection");
+		setSize(600, 100);
+		
+		setLayout(new GridLayout(1, colors.size()));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocation(20, 200);
+		
+		for(Color i : colors)
+		{
+			JPanel button = new JPanel();
+			button.setBackground(i);
+			add(button);
+		}
+		
+		setVisible(true);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		ArrayList <Color> c = new ArrayList<Color>();
+		
+		c.add(Color.black);
+		c.add(Color.blue);
+		c.add(Color.DARK_GRAY);
+		c.add(Color.GREEN);
+		c.add(Color.WHITE);
+		c.add(Color.pink);
+		
+		ColourPalette cp = new ColourPalette(c);
 	}
 
 }
